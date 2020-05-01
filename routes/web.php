5c1e141c0b1c62453ca51562c1fc42a1511410cb
Eddy Rufino <eddyjaair@gmail.com>
2020-05-01 11:14:03 -0500

    Funciona el login y logout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
